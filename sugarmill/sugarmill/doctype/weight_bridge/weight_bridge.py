511906fe0847dd4040dc8abfb825c051aaea7ca5
Pradip2113 <21pradipjadhav@gmail.com>
2023-02-01 13:54:10 +0530

    v2

---
+++ sugarmill/sugarmill/doctype/weight_bridge/weight_bridge.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, SugarMill and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class WeightBridge(Document):
+	pass
